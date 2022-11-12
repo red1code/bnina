600aa234b95784b17235c24a8fd1620dadc9936f
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-11-12 19:34:45 +0100

    setup notification reusable component + some styles

---
+++ src/app/shared/notification/notification.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NOTIFICATION_TYPES } from 'src/app/auth/models/notification';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss']
+})
+export class NotificationComponent implements OnInit {
+
+  types = NOTIFICATION_TYPES;
+  @Input() notificationType!: NOTIFICATION_TYPES;
+  @Input() notificationMessage!: string;
+  @Output() closeNotification = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showNotification(this.notificationType, this.notificationMessage);
+  }
+
+  showNotification(type: NOTIFICATION_TYPES, message: string) {
+    this.notificationType = type;
+    this.notificationMessage = message;
+  }
+
+  close() {
+    this.closeNotification.emit();
+  }
+
+}
